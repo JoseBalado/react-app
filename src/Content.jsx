c99b4c53d7722db298da762313d32b6484659793
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-27 21:30:50 +0100

    Show name of beers

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
 class Content extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         message: 'List of beers',
+         beers: []
+      }
+   }
+
+  async componentDidMount() {
+     try {
+       await axios.get(`https://api.punkapi.com/v2/beers`)
+        .then(res => {
+          const beers = res.data;
+          console.log(beers);
+          this.setState({ beers });
+        })
+
+      } catch(error) {
+         console.log(error);
+      }
+    }
+
    render() {
       return (
          <div>
             <h2>Content</h2>
             <p>The content text!!!</p>
+            <p>{this.state.message}</p>
+            {this.state.beers.map(beer => <div key={beer.id}>{beer.name}</div>)}
          </div>
       );
    }
