89b674914dfec8732028e2086d7d52013ef135fe
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-27 17:51:37 +0100

    Renaming files to jxs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header.js'
-import Content from './Content.js'
+import Header from './Header.jsx'
+import Content from './Content.jsx'
 
 class App extends React.Component {
    render() {
