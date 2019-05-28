72c17c890c30b708982f14d608a03882239e6f98
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-28 21:25:55 +0100

    Remove store and Todo component from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,6 @@ import React from 'react';
 import Header from './Header.js'
 import Main from './Main.js'
 
-import { connect } from 'react-redux'
-import { addTodo } from '../actions'
-
-import AddTodo from './AddTodo.js'
-import TodoList from './TodoList.js'
-
 class App extends React.Component {
    constructor(props) {
       super(props);
@@ -28,28 +22,16 @@ class App extends React.Component {
       this.setState({
          favoriteBeers: this.state.favoriteBeers.concat(this.state.beers.filter(beer => beer.id === id)[0])
       });
-
-      addTodo('hello')
-      console.log(this.state.favoriteBeers);
    }
 
    render() {
-      const { dispatch, visibleTodos } = this.props
       return (
          <div>
             <Header/>
             <Main />
-            <AddTodo onAddClick = {text =>dispatch(addTodo(text))} />
-            <TodoList todos = {visibleTodos}/>
          </div>
       );
    }
 }
 
-function select(state) {
-   return {
-      visibleTodos: state.todos
-   }
-}
-
-export default connect(select)(App);
+export default App;
