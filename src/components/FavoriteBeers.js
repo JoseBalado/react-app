4865d6c51b5afa2bd377ecbbb05382538384c43f
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-28 21:37:20 +0100

    Add AddTodo and TodoList component to FavoriteBeers.js

--- src/components/FavoriteBeers.js
+++ src/components/FavoriteBeers.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
+import { connect } from 'react-redux'
+import { addTodo } from '../actions'
+
+import AddTodo from './AddTodo.js'
+import TodoList from './TodoList.js'
+
 class FavoriteBeers extends React.Component {
    render() {
+      const { dispatch, visibleTodos } = this.props
+      console.log(this.props)
       return (
          <div>
             <h1>Favorite Beers</h1>
+            <AddTodo onAddClick = {text =>dispatch(addTodo(text))} />
+            <TodoList todos = {visibleTodos}/>
          </div>
       );
    }
 }
 
-export default FavoriteBeers
+function select(state) {
+   return {
+      visibleTodos: state.todos
+   }
+}
+
+export default connect(select)(FavoriteBeers);
