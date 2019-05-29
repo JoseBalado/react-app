01bf71563748a9cb761b9959adcdbca82a62f82f
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-29 23:06:20 +0100

    Fix className for Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ const Header = () => (
   <header>
     <nav>
       <ul className="nav">
-        <li class="nav-item">
+        <li className="nav-item">
           <Link className="nav-link" to='/'>Home</Link>
         </li>
-        <li class="nav-item">
+        <li className="nav-item">
           <Link className="nav-link" to='/favorites'>Favorite Beers</Link>
         </li>
       </ul>
