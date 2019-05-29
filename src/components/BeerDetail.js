44c3983a9c6238dd86324d13880ca0a336a88a2e
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-29 22:19:42 +0100

    Fix layout for BeerDetail

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -4,29 +4,27 @@ class Detail extends React.Component {
    render() {
       const beer = this.props.location.state.beer
       return (
-         <div>
-            <h2>Beer Detail</h2>
-            <div class="container">
-               <div class="row">
-                  <div class="col">
-                    Name:
-                  </div>
-                  <div class="col">
-                     <p>{beer.name}</p>
-                  </div>
+         <div class="container">
+            <h4>Beer Detail</h4>
+            <div class="row">
+               <div class="col">
+                  Name:
                </div>
-               <div class="row">
-                  <div class="col">
-                     Description:
-                  </div>
-                  <div class="col">
-                     <p>{beer.description}</p>
-                  </div>
+               <div class="col">
+                  <p>{beer.name}</p>
                </div>
-               <div class="row">
-                  <div class="col">
-                     <img src={beer.image_url} alt="beer" />
-                  </div>
+            </div>
+            <div class="row">
+               <div class="col">
+                  Description:
+               </div>
+               <div class="col">
+                  <p>{beer.description}</p>
+               </div>
+            </div>
+            <div class="row">
+               <div class="col">
+                  <img src={beer.image_url} alt="beer" />
                </div>
             </div>
          </div>
