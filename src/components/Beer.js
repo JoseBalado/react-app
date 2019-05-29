00e24d9202af01bc7a47fdbd795fe4123a04807e
Jose Balado <JoseBalado@users.noreply.github.com>
2019-05-29 22:47:19 +0100

    Rename class to className in Beer.js component

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -4,11 +4,11 @@ import { removeBeer } from '../store/actions';
 export default class Beer extends Component {
    render() {
       return (
-         <div class="row">
-            <div class="col">
+         <div className="row">
+            <div className="col">
                {this.props.name}
             </div>
-            <div class="col">
+            <div className="col">
                <button
                   className="btn btn-danger btn-sm"
                   onClick={(e) => this.props.dispatch(removeBeer(this.props.id))}
